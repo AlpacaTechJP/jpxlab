0e7549d3972095ffe4f0b3064428d9d51037ac64
Yuki Hayashi <yuki@alpaca.ai>
2019-09-23 18:40:01 +0000

    Polish the code

--- jpxlab/cli.py
+++ jpxlab/cli.py
@@ -7,7 +7,12 @@ import os
 import sys
 
 
-@click.command()
+@click.group()
+def cmd():
+    pass
+
+
+@cmd.command()
 @click.option(
     '-h', '--host', default="ftp.tmi.tse.or.jp",
     help='host name of the sftp server')
@@ -19,7 +24,7 @@ import sys
     help='user id to login to the sftp server')
 @click.option('-s', '--src', help='source path')
 @click.option('-o', '--out-dir', default="./", help='output directory')
-def main(host, port, user, src, out_dir):
+def fetch(host, port, user, src, out_dir):
     """Console script for jpxlab."""
 
     password = os.environ.get("JPXLAB_SFTP_PASS", None)
@@ -34,5 +39,45 @@ def main(host, port, user, src, out_dir):
     return 0
 
 
+@cmd.command()
+@click.option('-s', '--src', help='source path')
+@click.option('-o', '--out', help='output path')
+def resample(src, out):
+    """Resample the file fetched into 1sec aggregated dataframe"""
+
+    jpxlab.resample(src, out)
+
+    return 0
+
+
+@cmd.command()
+@click.option(
+    '-h', '--host', default="ftp.tmi.tse.or.jp",
+    help='host name of the sftp server')
+@click.option(
+    '-p', '--port', default=21, type=int,
+    help='port number of the sftp server')
+@click.option(
+    '-u', '--user',
+    help='user id to login to the sftp server')
+@click.option('-s', '--src', help='source path')
+@click.option('-o', '--out-dir', default="./", help='output directory')
+def fetch_resample(host, port, user, src, out_dir):
+    """Fetch and resample"""
+
+    password = os.environ.get("JPXLAB_SFTP_PASS", None)
+    if password is None:
+        password = click.prompt(
+            'Please enter a password for the sftp server', type=str)
+
+    sftp = jpxlab.get_sftp_session(host, port, user, password)
+
+    out_filename = jpxlab.fetch_and_convert(sftp, src, out_dir)
+
+    jpxlab.resample(out_filename, out_filename.replace("_raw.h5", ".h5"))
+
+    return 0
+
+
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    sys.exit(cmd())
